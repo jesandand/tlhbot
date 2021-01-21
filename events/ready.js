47f3dd3a482fbaa2059c4930434b3f7bc48d367c
jesandand <75883229+jesandand@users.noreply.github.com>
2021-01-21 23:32:12 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Development By Jesand Andriand | Wonderful Dream", {
+  await client.user.setActivity("Development By Jesand Andriand | Night Land", {
     type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
