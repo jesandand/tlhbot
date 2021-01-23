89b4b7fb10e9ba326aea6d3070e0112b954f27e1
jesandand <75883229+jesandand@users.noreply.github.com>
2021-01-24 02:34:31 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("The Little Heaven | The Perfect Place To Find Happiness", {
+  await client.user.setActivity("The Little Heaven | The Perfect Place To Find Happiness | By Jesand Andriand", {
     type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
