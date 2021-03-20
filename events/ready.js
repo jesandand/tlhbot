c0a78a21f517ef893000c3d4050314216d6c520a
jesandand <75883229+jesandand@users.noreply.github.com>
2021-03-21 03:45:10 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(". The Little Heaven | Dev By Jesand", {
-    type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity(" Aftershine Entertainment | Developer By Jesand", {
+    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
