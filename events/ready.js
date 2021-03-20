25028110875d39aa8d78e3be1d22eca46b91d25a
jesandand <75883229+jesandand@users.noreply.github.com>
2021-03-21 04:23:26 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" Aftershine Entertainment | Developer By Jesand", {
+  await client.user.setActivity(" Aftershine Entertainment | Developer By Jesand ", {
     type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
