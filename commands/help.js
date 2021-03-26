9a03523e54d45dcfbe79abc4b3f2541380ef6625
jesandand <75883229+jesandand@users.noreply.github.com>
2021-03-26 22:44:50 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,7 +20,7 @@ module.exports = {
         .setAuthor("Commands bot "+client.user.username, "https://cdn.discordapp.com/attachments/822042698152280105/823023536296820776/1-2.jpg")
         .setColor("BLUE")
         .setDescription(allcmds)
-        .setFooter(`Aftershine Entertainment ${client.config.prefix}help [command] | Development By Jesand`)
+        .setFooter(`Aftershine Entertainment ${client.config.prefix}help | Development By Jesand`)
 
         if(!args[0])return message.channel.send(embed)
         else {
