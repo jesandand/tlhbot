8364c39ebed6e1fa180362792b43acaabb1848a6
jesandand <75883229+jesandand@users.noreply.github.com>
2021-03-31 14:29:54 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,17 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" Aftershine Entertainment | as.help Info Commands" , {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
+
+    let statuses = [
+        `as.help | Aftershine Entertainment`,
+        `as.help | Punten`,
+        `as.help | Paket`,
+        `as.help | Develop By Jessxyz`,
+        `as.help | Discord.io/Aftershines`
+    ];
+
+    setInterval(function() {
+
+        let status = statuses[Math.floor(Math.random() * statuses.length)]
+        client.user.setPresence({ activity: { name: status, type: "STREAMING"}, status: 'online'});
+    }, 1000);
 };
