5d1119b173c77e43da649bf56fcba787bfa6dadf
jesandand <75883229+jesandand@users.noreply.github.com>
2021-03-31 13:41:27 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" Aftershine Entertainment | ${await client.guilds.cache.size} | Users : ${await client.users.cache.size}" , {
+  await client.user.setActivity(" Aftershine Entertainment | Server : ${client.guilds.cache.size} | Member : ${client.users.cache.size}" , {
     type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
