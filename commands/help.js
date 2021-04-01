915fc81f854063ee7437fb01143f1bb1dd9c4dde
jesandand <75883229+jesandand@users.noreply.github.com>
2021-04-02 02:36:43 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,6 +20,7 @@ module.exports = {
         .setAuthor("Commands bot "+client.user.username, "https://cdn.discordapp.com/attachments/822042698152280105/823023536296820776/1-2.jpg")
         .setColor("BLUE")
         .setDescription(allcmds)
+        .addField(" <a:9_Style_Discord:789405103303950366>  •  Informasi" , "```\nhelp, invite```")
         .addField(" <a:as:827256384541687859>  •  Music Commands" , "```\nafk, play, pause, resume, volume, loop, \nlyrics, nowplaying, playlist , queue, stop, \nremove, search, shuffle, skip, skipto```")
         .setImage("https://cdn.discordapp.com/attachments/822042698152280105/823022484630405190/aftershine.jpg")
         .setFooter(`Aftershine Entertainment ${client.config.prefix}help | Development By Jesand`)
