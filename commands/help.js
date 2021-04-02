b0d6b9bee3f53e519ebfde2aa00286968ca732d9
jesandand <75883229+jesandand@users.noreply.github.com>
2021-04-02 13:02:45 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,10 +20,10 @@ module.exports = {
         .setAuthor("."+client.user.username, "https://cdn.discordapp.com/attachments/822042698152280105/823023536296820776/1-2.jpg")
         .setColor("BLUE")
         .setDescription(allcmds)
-        .addField(" \n Commands List After's Entertainment\n")
+        .addField("\nCommands List After's Entertainment\n")
         .addField(" <a:9_Style_Discord:789405103303950366>  •  Informasi" , "```\nhelp, invite, afk```")
         .addField(" <a:as:827256384541687859>  •  Music Commands" , "```\nplay, pause, resume, \nvolume, nowplaying, stop, \nskip, skipto```")
-        .addField(" <a:as:827256384541687859>  •  Music Commands 2" , "```\nloop, lyrics, playlist, \nqueue, search, shuffle, \nremove \n```")
+        .addField(" <a:as:827256384541687859>  •  Music Commands 2" , "```\nloop, lyrics, playlist, \nqueue, search, shuffle, \nremove```")
         .setImage("https://cdn.discordapp.com/attachments/822042698152280105/823022484630405190/aftershine.jpg")
         .setFooter(`Aftershine Entertainment ${client.config.prefix}help | Development By Jesand`)
 
